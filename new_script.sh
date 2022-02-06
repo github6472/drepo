15935a4c844fa955514e07ec5a1ec35aec1bfd57
neelufars <shaikneelufar599@gmail.com>
2022-02-06 20:19:26 +0530

    updated shell script

--- new_script.sh
+++ new_script.sh
@@ -9,7 +9,7 @@ do
    echo "============== creating container $i =============="
    docker run -itd --name ${name}$i --hostname ${name}$i -v /home/workspace/dev-data:/home/workspace/docker_data -p 80${pn}$i:8080 neelus/myubuntu:latest /bin/bash
    echo "$(docker inspect --format '{{.NetworkSettings.Networks.bridge.IPAddress}}' ${name}$i)"
-   docker cp /home/neelufar/.jenkins/workspace/cont-deploy/target/drepo.war ${name}$i:/apache-tomcat-9.0.58/webapps
+   docker cp /home/neelufar/.jenkins/workspace/cont-deploy/target/abcd.war ${name}$i:/apache-tomcat-9.0.58/webapps
    i=$(($i+1))
 
 done
